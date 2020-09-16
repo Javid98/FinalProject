c106dab1c7d820d2d48c19d79dae44278a4743bc
Javid <javidia@code.edu.az>
2020-09-16 23:14:33 +0400

    End of Final Project

--- FinalProject/Controllers/BestSellersController.cs
+++ FinalProject/Controllers/BestSellersController.cs
@@ -26,10 +26,6 @@ namespace FinalProject.Controllers
 			List<Book> books = _db.Books.Include(b=>b.BookAuthors).ThenInclude(b=>b.Author).Include(b=>b.BookCategories).Include(b=>b.Publisher).OrderByDescending(b => b.SaleCount).Take(100).ToList();
 			ViewBag.PageCount = Math.Ceiling((decimal)books.Count() / 10);
 			List<Book> bestSellers = books.Skip((page - 1) * 10).Take(10).ToList();
-			//List<BookAuthor> bookAuthors = _db.BookAuthors.ToList();
-			//List<Author> authors = _db.Authors.OrderBy(n => n.Fullname).ToList();
-			//List<Publisher> publishers = _db.Publishers.OrderByDescending(n => n.BookCount).ToList();
-			//List<Category> categories = _db.Categories.OrderBy(n => n.Name).ToList();
 			ViewBag.Currency = _db.Bios.FirstOrDefault().Currency;
 			AppUser user = new AppUser();
 			if (User.Identity.IsAuthenticated)
@@ -47,10 +43,6 @@ namespace FinalProject.Controllers
 			{
 				Books = bestSellers,
 				AllBooks=books,
-				//BookAuthors = bookAuthors,
-				//Authors = authors,
-				//Publishers = publishers,
-				//Categories = categories,
 				BookInCarts = bookInCarts,
 				FavoriteBooks = favoriteBooks
 			};
